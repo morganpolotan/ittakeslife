48064faa6e044bd5f3ef32963bb25553470d20e4
Morgan Polotan <morgan.polotan@tapad.com>
2014-07-14 22:34:38 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yolifeApp
+ * @description
+ * # yolifeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yolifeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yolifeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yolifeApp
+ */
+angular.module('yolifeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
